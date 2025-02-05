7e08816c9c2cec7e74647c92612cc1d52bad7f2a
zummon <zummon.space@gmail.com>
2025-02-05 07:04:52 +0700

    save

--- src/App.svelte
+++ src/App.svelte
@@ -52,7 +52,23 @@
     return amount;
   })
 
-  function done () {
+  function formatNumber (number, option) {
+    return number == 0 ? "" : number.toLocaleString(qry.lang, { minimumFractionDigits: 2, maximumFractionDigits: 2, ...option });
+  }
+  function formatDate (value, option) {
+    return new Date(value).toLocaleDateString(qry.lang, { day: 'numeric', month: 'short', year: 'numeric' , ...option })
+  }
+
+	function addRow () {
+		qry.price.push(''); 
+		qry.qty.push(''); 
+		qry.desc.push(''); 
+	}
+  function clearForm () {
+    qry = { ...qry, lang: qry.lang, doc: qry.doc };
+  }
+
+  function saveLink () {
     const searchParams = new URLSearchParams();
     let vendorLogo = "";
     if (qry.vendorLogo.length > 100) {
@@ -71,16 +87,6 @@
       "https://codepen.io/zummon/full/wvLrqBe?" + searchParams.toString()
     );
   }
-  function clear () {
-    qry = { ...qry, lang: qry.lang, doc: qry.doc };
-  }
-  function formatNumber (number, option) {
-    return number == 0 ? "" : number.toLocaleString(qry.lang, { minimumFractionDigits: 2, maximumFractionDigits: 2, ...option });
-  }
-  function formatDate (value, option) {
-    return new Date(value).toLocaleDateString(qry.lang, { day: 'numeric', month: 'short', year: 'numeric' , ...option })
-  }
-  
   onMount(() => {
     const searchParams = new URLSearchParams(location.search);
 
@@ -268,23 +274,19 @@
 </div>
 
 <div class="flex flex-wrap justify-center px-2 py-4 lg:py-6 gap-4 print:hidden">
-  <button class="text-green-500 cursor-pointer" title="Add a new row" onclick={() => { 
-		qry.price.push(''); 
-		qry.qty.push(''); 
-		qry.desc.push(''); 
-	}}>
+  <button class="text-green-500 cursor-pointer" title="Add a new row" onclick={() => { addRow() }}>
 		<!-- https://heroicons.com/micro plus -->
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-8">
 			<path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
 		</svg>
 	</button>
-  <button class="text-neutral-500 cursor-pointer" title="Clear forms" onclick={() => { clear() }}>
+  <button class="text-neutral-500 cursor-pointer" title="Clear forms" onclick={() => { clearForm() }}>
 		<!-- https://heroicons.com/solid trash -->
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
 			<path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
 		</svg>
 	</button>
-  <button class="text-cyan-500 cursor-pointer" title="Copy" onclick={() => { done(); }}>
+  <button class="text-cyan-500 cursor-pointer" title="Copy" onclick={() => { saveLink() }}>
 		<!-- https://heroicons.com/solid document-duplicate -->
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
 			<path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 0 1 3.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0 1 21 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 0 1 7.5 16.125V3.375Z" />
